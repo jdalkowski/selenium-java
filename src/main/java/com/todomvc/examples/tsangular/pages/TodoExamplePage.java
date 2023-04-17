e48f8aadbdcd247012c725206e523571a3d81b88
Jaroslaw Dalkowski <jaroslaw.dalkowski@gmail.com>
2023-04-17 09:59:48 +0200

    Add new tests and methods

--- src/main/java/com/todomvc/examples/tsangular/pages/TodoExamplePage.java
+++ src/main/java/com/todomvc/examples/tsangular/pages/TodoExamplePage.java
@@ -2,9 +2,11 @@ package com.todomvc.examples.tsangular.pages;
 
 import io.qameta.allure.Step;
 import lombok.Getter;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
@@ -21,6 +23,9 @@ public class TodoExamplePage extends WebPage {
     @FindBy(css = "ul.todo-list>li")
     private List<WebElement> todoElementList;
 
+    @FindBy(css = "span.todo-count>strong")
+    private WebElement itemsLeftCounter;
+
     public TodoExamplePage(final WebDriver driver) {
         super(driver);
     }
@@ -29,5 +34,29 @@ public class TodoExamplePage extends WebPage {
     public void addTodoNote (String noteText) {
         todoInput.sendKeys(noteText);
         todoInput.sendKeys(Keys.ENTER);
+    }
+
+    @Step
+    public void markNoteAsCompleted(int index) {
+        todoElementList.get(index).findElement(By.tagName("input")).click();
+    }
+
+    @Step
+    public int getItemsLeft() {
+        return Integer.parseInt(itemsLeftCounter.getText());
+    }
+
+    @Step
+    public int countNumberOfCompletedItems() {
+        return (int) todoElementList
+                .stream()
+                .filter(e -> e.getAttribute("class").contains("completed"))
+                .count();
+    }
+
+    public void removeTodoNote(int elementIndex) {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(todoElementList.get(elementIndex)).build().perform();
+        driver.findElements(By.cssSelector("button.destroy")).get(elementIndex).click();
     }
 }
--- src/test/java/com/todomvc/examples/tsangular/tests/TodoMvcTest.java
+++ src/test/java/com/todomvc/examples/tsangular/tests/TodoMvcTest.java
@@ -2,24 +2,53 @@ package com.todomvc.examples.tsangular.tests;
 
 import com.todomvc.examples.tsangular.core.TestCase;
 import com.todomvc.examples.tsangular.pages.TodoExamplePage;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TodoMvcTest extends TestCase {
 
+    private static final String firstNote = "Test note number one";
+    private static final String secondNote = "Test note number two";
+    private static final String thirdNote = "Test note number three";
+
     private TodoExamplePage todoExamplePage;
 
-    @BeforeClass
+    @BeforeMethod
     public void prepareTest() {
         todoExamplePage = openTodoExamplePage();
     }
 
     @Test
     public void shouldAddTodoNotes() {
-        todoExamplePage.addTodoNote("first note");
-        todoExamplePage.addTodoNote("second note");
-        todoExamplePage.addTodoNote("third note");
+        addThreeTodoItems();
         assertThat(todoExamplePage.getTodoElementList().size()).isEqualTo(3);
+        assertThat(todoExamplePage.getTodoElementList().get(0).getText()).isEqualTo(firstNote);
+        assertThat(todoExamplePage.getTodoElementList().get(1).getText()).isEqualTo(secondNote);
+        assertThat(todoExamplePage.getTodoElementList().get(2).getText()).isEqualTo(thirdNote);
+    }
+
+    @Test
+    public void shouldMarkNoteAsCompleted() {
+        addThreeTodoItems();
+        todoExamplePage.markNoteAsCompleted(0);
+        assertThat(todoExamplePage.countNumberOfCompletedItems()).isEqualTo(1);
+        assertThat(todoExamplePage.getItemsLeft()).isEqualTo(todoExamplePage.getTodoElementList()
+                .size()-todoExamplePage.countNumberOfCompletedItems());
+    }
+
+    @Test
+    public void shouldRemoveTodoNoteFromList() {
+        addThreeTodoItems();
+        assertThat(todoExamplePage.getTodoElementList().size()).isEqualTo(3);
+        todoExamplePage.removeTodoNote(1);
+        assertThat(todoExamplePage.getTodoElementList().size()).isEqualTo(2);
+    }
+
+    private void addThreeTodoItems() {
+        todoExamplePage.addTodoNote(firstNote);
+        todoExamplePage.addTodoNote(secondNote);
+        todoExamplePage.addTodoNote(thirdNote);
     }
 }
